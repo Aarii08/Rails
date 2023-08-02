0a532d117f988538f199c1768e48cd747e056017
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-08-02 04:35:18 +0000

    [Finish] 2-5

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_02_034241) do
+ActiveRecord::Schema.define(version: 2023_08_02_041933) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
